f167a9a41ddfcfb656f6a2b1fdbd5b7305b30238
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-15 01:34:40 -0700

    make it red

--- events.js
+++ events.js
@@ -1,1 +1,14 @@
 // Your code here
+addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded');
+  const redInput = document.querySelector('#red-input');
+  //   debugger;
+  redInput.addEventListener('change', e => {
+    console.log(e.target.value);
+    if (e.target.value.indexOf('red') > -1) {
+      redInput.style.setProperty('background', 'red');
+    } else {
+      redInput.style.setProperty('background', '');
+    }
+  });
+});
