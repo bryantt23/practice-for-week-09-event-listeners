5f86bf165349fed4bb5efb681e64413de801130c
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-15 21:22:52 -0700

    remove listeners not working

--- events.js
+++ events.js
@@ -7,25 +7,37 @@ addEventListener('DOMContentLoaded', () => {
   const listAdd = document.querySelector('#list-add');
   const section3 = document.querySelector('#section-3');
   const colorSelect = document.querySelector('#color-select');
+  const removeListeners = document.querySelector('#remove-listeners');
 
-  redInput.addEventListener('change', e => {
-    console.log(e.target.value);
+  function addLi() {
+    const li = document.createElement('li');
+    li.textContent = listAdd.value;
+    ul.appendChild(li);
+  }
+
+  function changeBackgroundColor(e) {
     if (e.target.value.indexOf('red') > -1) {
       redInput.style.setProperty('background', 'red');
     } else {
       redInput.style.setProperty('background', '');
     }
-  });
+  }
+
+  function useColorSelect(e) {
+    section3.style.backgroundColor = e.target.value;
+  }
+
+  redInput.addEventListener('change', e => changeBackgroundColor(e), true);
   listAdd.addEventListener('change', e => {
     console.log(e.target.value);
   });
-  addItem.addEventListener('click', () => {
-    const li = document.createElement('li');
+  addItem.addEventListener('click', addLi);
+  colorSelect.addEventListener('change', e => useColorSelect(e), true);
 
-    li.textContent = listAdd.value;
-    ul.appendChild(li);
-  });
-  colorSelect.addEventListener('change', e => {
-    section3.style.backgroundColor = e.target.value;
+  removeListeners.addEventListener('click', () => {
+    console.log('remove event listeners');
+    redInput.removeEventListener('change', changeBackgroundColor, true);
+    addItem.removeEventListener('click', addLi);
+    colorSelect.removeEventListener('change', useColorSelect, true);
   });
 });
