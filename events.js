97fd38e75525cf1938b49702df9e285e3c6ddae4
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-15 20:50:16 -0700

    add li

--- events.js
+++ events.js
@@ -2,6 +2,10 @@
 addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
   const redInput = document.querySelector('#red-input');
+  const addItem = document.querySelector('#add-item');
+  const ul = document.querySelector('ul');
+  const listAdd = document.querySelector('#list-add');
+
   //   debugger;
   redInput.addEventListener('change', e => {
     console.log(e.target.value);
@@ -10,5 +14,14 @@ addEventListener('DOMContentLoaded', () => {
     } else {
       redInput.style.setProperty('background', '');
     }
+  });
+  listAdd.addEventListener('change', e => {
+    console.log(e.target.value);
+  });
+  addItem.addEventListener('click', () => {
+    const li = document.createElement('li');
+
+    li.textContent = listAdd.value;
+    ul.appendChild(li);
   });
 });
